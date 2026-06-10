1d723402e0b0c5c0e9c4874bfabeebca3b6b10ea
nancysheikh <fizzahahsan57@gmail.com>
2026-06-10 11:16:02 +0500

    Frontend homepage complete - by Fizza

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.scan import scan_bp
 from routes.auth import auth_bp
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(scan_bp, url_prefix='/scan')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
